6841eb4c207eb2a66fc80c62b92cdc8ed386138d
Yu Zhou <2291764902@qq.com>
2019-08-29 18:54:44 +0800

    changed code

--- Segmentation Interaction/optimization.cpp
+++ Segmentation Interaction/optimization.cpp
@@ -212,11 +212,207 @@ double Optimization::D_compute(double angle,vector<vector<Point2f>>& seg_contour
 	return Deformation;
 }
 
-
-vector<vector<Point2f>> Seg_compute(vector<vector<Point2f>> segment,vector<Point2f> contour)
+double crosspruduct(Point2f A,Point2f B)
+{
+	return A.x*B.y - A.y*B.x;
+}
+bool is_in_contour(Point2f mid, vector<Point2f> contour)
+{
+	//判断点mid是否在contour中，在则返回true
+	int n = contour.size();
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (mid.y == contour[i].y&&mid.x == contour[i].x)
+			return false;
+		else if (mid.y == contour[i].y&&mid.y == contour[(i + 1) % n].y)
+		{
+			if ((mid.x >= contour[i].x&&mid.x <= contour[(i + 1) % n].x) || (mid.x <= contour[i].x&&mid.x >= contour[(i + 1) % n].x))
+				return false;
+		}
+		else if ((mid.y >= contour[i].y&&mid.y < contour[(i + 1) % n].y) || (mid.y >= contour[(i + 1) % n].y&&mid.y < contour[i].y))
+		{
+			double d = ((mid.y - contour[i].y)*(contour[(i + 1) % n].x - contour[i].x) + (contour[i].x - mid.x)*(contour[(i + 1) % n].y - contour[i].y))
+				* (contour[(i + 1) % n].y - contour[i].y);
+			if (d > 0)
+				count++;
+			else if (d == 0)
+				return false;
+		}
+	}
+	if (count % 2 == 0)
+		return false;
+	else
+		return true;//在contour中
+}
+vector<vector<Point2f>> Seg_compute(vector<vector<Point2f>> segment,vector<Point2f> contour,vector<vector<int>>&  intersectint_index)
 {
 	//segment是一个二维数组，数组为2列n行，n表示分割线数量，数组第一列表示分割线上某一点，数组第二列表示分割线的方向向量
 	//contour表示多边形轮廓
+	/*
+	对于每条segment，计算其与多边形边的交点
+	计算交点的时候，是直线与线段的相交，当他们重合的时候，不需要将线段的两个端点加入进交点集合中，直接忽略即可；
+	如果两个交点之间有多边形顶点，则观察多边形方向，添加分割线到segment-pair；
+	*/
+	vector<vector<Point2f>> segment_pairs;
+	vector<int> no_intersect;//存储与多边形无交点的分割线在segment中的下标
+	for (int i = 0; i < segment.size(); i++)
+	{
+		int index = i;
+		vector<Point2f> intersecting;//存放分割线与多边形边的交点的集合
+		vector<vector<int>> intersect_index;//存放分割线与多边形边交点所处多边形边两端点的index,放置顺序是多边形顶点的顺序
+		for (int j = 0; j < contour.size(); j++)
+		{
+			//e0和e1表示线段的两个端点下标
+			int e0 = j;
+			int e1 = (j + 1)%contour.size();
+			Point2f P1 = contour[e0];
+			Point2f d1;//t要求是大于等于0小于等于1
+			d1.x = contour[e1].x - contour[e0].x;
+			d1.y = contour[e1].y - contour[e0].y;
+			Point2f P0 = segment[index][0];
+			Point2f d0 = segment[index][1];
+			double K = crosspruduct(d0, d1);
+			Point2f deta;
+			deta.x = P1.x - P0.x;
+			deta.y = P1.y - P0.y;
+			if (K == 0)
+			{
+				//说明平行或重合
+				continue;
+			}
+			else
+			{
+				double t = crosspruduct(deta, d0) / K;
+				if (t >= 0 && t <= 1)
+				{
+					//说明分割线与多边形的边相交
+					Point2f intersect;
+					intersect = P1 + t*d1;
 
-	
+					//首先应该确定现有的交点中是否已经包含该交点，如果已经包含，则直接忽略；
+					vector<Point2f> ::iterator iter;
+					iter = find(intersecting.begin(), intersecting.end(), intersect);
+					if (iter == intersecting.end())
+					{
+						intersecting.push_back(intersect);
+
+						vector<int> temp;
+
+						temp.push_back(e0);
+						temp.push_back(e1);
+
+						intersect_index.push_back(temp);
+					}
+					else
+						continue;
+					
+				}
+				else
+					continue;
+			}
+		}
+		//下面应该抽取segment中下标为i的直线对应多边形形成的分割线集合
+		if (intersecting.size() == 0||intersecting.size()==1)
+		{
+			//分割线i对多边形必定无分割
+			no_intersect.push_back(i);
+		}
+		else
+		{
+			//将交点按照x轴坐标排序
+			int len = intersecting.size();
+			if (len > 1 && intersecting[0].x == intersecting[1].x)
+			{
+				//如果所有交点的横坐标相等，也就是分割线垂直于x轴，那么久按照y坐标排序
+				for (int s = 0; s < len - 1; s++)
+				{
+					for (int w = 0; w < len - 1 - s; w++)
+					{
+						if (intersecting[w].y > intersecting[w + 1].y)
+						{
+							Point2f temp = intersecting[w + 1];
+							intersecting[w + 1] = intersecting[w];
+							intersecting[w] = temp;
+							vector<int> temp0;
+							temp0.assign(intersect_index[w + 1].begin(), intersect_index[w + 1].end());
+							intersect_index[w + 1].clear();
+							intersect_index[w + 1].assign(intersect_index[w].begin(), intersect_index[w].end());
+							intersect_index[w].clear();
+							intersect_index[w].assign(temp0.begin(), temp0.end());
+						}
+					}
+				}
+			}
+			else
+			{
+				//否则按照x坐标排序
+				for (int s = 0; s < len - 1; s++)
+				{
+					for (int w = 0; w < len - 1 - s; w++)
+					{
+						if (intersecting[w].x > intersecting[w + 1].x)
+						{
+							Point2f temp = intersecting[w + 1];
+							intersecting[w + 1] = intersecting[w];
+							intersecting[w] = temp;
+							vector<int> temp0;
+							temp0.assign(intersect_index[w + 1].begin(), intersect_index[w + 1].end());
+							intersect_index[w + 1].clear();
+							intersect_index[w + 1].assign(intersect_index[w].begin(), intersect_index[w].end());
+							intersect_index[w].clear();
+							intersect_index[w].assign(temp0.begin(), temp0.end());
+						}
+					}
+				}
+			}
+			//根据线段中点是否在多边形内部来判断哪些是新生成的分割线
+			int number = 0;
+			for (int s = 0; s < intersecting.size()-1; s++)
+			{
+				int w = s + 1;
+				Point2f midpoint;
+				midpoint.x = (intersecting[s].x + intersecting[w].x) / 2;
+				midpoint.y = (intersecting[s].y + intersecting[w].y) / 2;
+
+				if (is_in_contour(midpoint,  contour))
+				{
+                    //说明分割线的中点在多边形内部，也就是该线段是分割线
+					number++;
+					vector<Point2f> temp;
+					temp.push_back(intersecting[s]);
+					temp.push_back(intersecting[w]);
+					segment_pairs.push_back(temp);
+					vector<int> temp0;
+					temp0.push_back(intersect_index[s][0]);
+					temp0.push_back(intersect_index[s][1]);
+					temp0.push_back(intersect_index[w][0]);
+					temp0.push_back(intersect_index[w][0]);
+					intersectint_index.push_back(temp0);
+				}
+				else
+				{
+
+				}
+			}
+			if (number == 0)
+			{
+				//分割线i对多边形必定无分割
+				no_intersect.push_back(i);
+			}
+		}
+	}
+	return segment_pairs;
+}
+
+/*
+优化中的约束：
+1.如果直线与多变形交点为0，
+2.如果两个分割线的交点在多边形内部，
+3.如果生成的新轮廓放不下轮廓，
+
+*/
+void  myconstraint1()
+{
+
 }
